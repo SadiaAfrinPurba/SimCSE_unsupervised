5144e62f6e5e8ffb48b62faaa71a37d03fae9832
SadiaAfrinPurba <purba0101@gmail.com>
2023-05-20 20:56:17 +0600

    Initial pseudo coding and add CNN model

--- model.py
+++ model.py
@@ -1,14 +1,16 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import transformers
 from transformers.models.bert.modeling_bert import BertPreTrainedModel, BertModel
 from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutputWithPoolingAndCrossAttentions
+import torch.nn.functional as F
+
 
 class MLPLayer(nn.Module):
     """
     Head for getting sentence representations over RoBERTa/BERT's CLS representation.
     """
-    
 
     def __init__(self, config):
         super().__init__()
@@ -21,7 +23,7 @@ class MLPLayer(nn.Module):
 
         return x
 
-        
+
 class Similarity(nn.Module):
     """
     Dot product or cosine similarity
@@ -35,6 +37,7 @@ class Similarity(nn.Module):
     def forward(self, x, y):
         return self.cos(x, y) / self.temp
 
+
 class Pooler(nn.Module):
     """
     Parameter-free poolers to get the sentence embedding
@@ -44,10 +47,12 @@ class Pooler(nn.Module):
     'avg_top2': average of the last two layers.
     'avg_first_last': average of the first and the last layers.
     """
+
     def __init__(self, pooler_type):
         super().__init__()
         self.pooler_type = pooler_type
-        assert self.pooler_type in ["cls", "cls_before_pooler", "avg", "avg_top2", "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
+        assert self.pooler_type in ["cls", "cls_before_pooler", "avg", "avg_top2",
+                                    "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
 
     def forward(self, attention_mask, outputs):
         last_hidden = outputs.last_hidden_state
@@ -59,13 +64,15 @@ class Pooler(nn.Module):
         else:
             raise NotImplementedError
 
+
 class Arguments():
-    def __init__(self): 
-        self.temp = 0.05 # Temperature for softmax.
-        self.pooler_type = 'cls' #What kind of pooler to use 
+    def __init__(self):
+        self.temp = 0.05  # Temperature for softmax.
+        self.pooler_type = 'cls'  # What kind of pooler to use
         # Number of sentences in one instance
         # 2: pair instance; 3: pair instance with a hard negative
         self.num_sent = 2
+
 
 args = Arguments()
 
@@ -80,23 +87,23 @@ def cl_init(cls, config):
     cls.sim = Similarity(temp=args.temp)
     cls.init_weights()
 
+
 def cl_forward(cls,
-    encoder,
-    input_ids=None,
-    attention_mask=None,
-    token_type_ids=None,
-    position_ids=None,
-    head_mask=None,
-    inputs_embeds=None,
-    labels=None,
-    output_attentions=None,
-    output_hidden_states=None,
-    return_dict=None,
-):
-    #import ipdb; ipdb.set_trace();
+               encoder,
+               input_ids=None,
+               attention_mask=None,
+               token_type_ids=None,
+               position_ids=None,
+               head_mask=None,
+               inputs_embeds=None,
+               labels=None,
+               output_attentions=None,
+               output_hidden_states=None,
+               return_dict=None,
+               ):
+    # import ipdb; ipdb.set_trace();
     return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
-    batch_size = int(input_ids.size(0)/2)
-    
+    batch_size = int(input_ids.size(0) / 2)
 
     # mlm_outputs = None
     # Flatten input for encoding
@@ -106,6 +113,9 @@ def cl_forward(cls,
     #     token_type_ids = token_type_ids.view((-1, token_type_ids.size(-1))) # (bs * num_sent, len)
 
     # Get raw embeddings
+    # BAseModelOutputWithPoolingAndCrossAttentions
+    # pooler_output shape: (64, 768)
+    # last hidden state shape: (64, 32, 768)
     outputs = encoder(
         input_ids,
         attention_mask=attention_mask,
@@ -118,9 +128,9 @@ def cl_forward(cls,
         return_dict=True,
     )
 
-    # Pooling
+    # Pooling shape (32, 2, 768)
     pooler_output = cls.pooler(attention_mask, outputs)
-    pooler_output = pooler_output.view((batch_size, args.num_sent, pooler_output.size(-1))) # (bs, num_sent, hidden)
+    pooler_output = pooler_output.view((batch_size, args.num_sent, pooler_output.size(-1)))  # (bs, num_sent, hidden)
 
     # If using "cls", we add an extra MLP layer
     # (same as BERT's original implementation) over the representation.
@@ -128,7 +138,18 @@ def cl_forward(cls,
         pooler_output = cls.mlp(pooler_output)
 
     # Separate representation
-    z1, z2 = pooler_output[:,0], pooler_output[:,1]
+    # TODO: Change here
+    # z1, z2 shape: (32, 768)
+    z1, z2 = pooler_output[:, 0], pooler_output[:, 1]
+
+    cnn_output = CNN()
+    # vae_output = VAE(cnn_output) # (32, emb_size)
+
+    # similarity_matrix
+    z1, z2 = get_most_similar_emb(vae_output)
+
+    # backtrack z1, z2 to bert emb
+
     cos_sim = cls.sim(z1.unsqueeze(1), z2.unsqueeze(0))
     labels = torch.arange(cos_sim.size(0)).long().to(cls.device)
     loss_fct = nn.CrossEntropyLoss()
@@ -145,21 +166,21 @@ def cl_forward(cls,
         attentions=outputs.attentions,
     )
 
-def sentemb_forward(
-    cls,
-    encoder,
-    input_ids=None,
-    attention_mask=None,
-    token_type_ids=None,
-    position_ids=None,
-    head_mask=None,
-    inputs_embeds=None,
-    labels=None,
-    output_attentions=None,
-    output_hidden_states=None,
-    return_dict=None,
-):
 
+def sentemb_forward(
+        cls,
+        encoder,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+):
     return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
 
     outputs = encoder(
@@ -193,48 +214,106 @@ class BertForCL(BertPreTrainedModel):
 
     def __init__(self, config):
         super().__init__(config)
-        #self.model_args = model_kargs["model_args"]
+        # self.model_args = model_kargs["model_args"]
         self.bert = BertModel(config)
         cl_init(self, config)
 
     def forward(self,
-        input_ids=None,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
-        labels=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-        sent_emb=False,
-    ):
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                labels=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None,
+                sent_emb=False,
+                ):
         if sent_emb:
             return sentemb_forward(self, self.bert,
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                position_ids=position_ids,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                labels=labels,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
+                                   input_ids=input_ids,
+                                   attention_mask=attention_mask,
+                                   token_type_ids=token_type_ids,
+                                   position_ids=position_ids,
+                                   head_mask=head_mask,
+                                   inputs_embeds=inputs_embeds,
+                                   labels=labels,
+                                   output_attentions=output_attentions,
+                                   output_hidden_states=output_hidden_states,
+                                   return_dict=return_dict,
+                                   )
         else:
             return cl_forward(self, self.bert,
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                position_ids=position_ids,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                labels=labels,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
+                              input_ids=input_ids,
+                              attention_mask=attention_mask,
+                              token_type_ids=token_type_ids,
+                              position_ids=position_ids,
+                              head_mask=head_mask,
+                              inputs_embeds=inputs_embeds,
+                              labels=labels,
+                              output_attentions=output_attentions,
+                              output_hidden_states=output_hidden_states,
+                              return_dict=return_dict,
+                              )
 
 
+class CNN(nn.Module):
+
+    def __init__(self,
+                 pretrained_embedding=None,
+                 freeze_embedding=False,
+                 vocab_size=None,
+                 embed_dim=300,
+                 filter_sizes=[3, 4, 5],
+                 num_filters=[100, 100, 100],
+                 num_classes=2,
+                 dropout=0.5):
+
+        super(CNN, self).__init__()
+        # Embedding layer
+        # if pretrained_embedding is not None:
+        #     self.vocab_size, self.embed_dim = pretrained_embedding.shape
+        #     self.embedding = nn.Embedding.from_pretrained(pretrained_embedding,
+        #                                                   freeze=freeze_embedding)
+        # else:
+        #     self.embed_dim = embed_dim
+        #     self.embedding = nn.Embedding(num_embeddings=vocab_size,
+        #                                   embedding_dim=self.embed_dim,
+        #                                   padding_idx=0,
+        #                                   max_norm=5.0)
+        # Conv Network
+        self.conv1d_list = nn.ModuleList([
+            nn.Conv1d(in_channels=self.embed_dim,
+                      out_channels=num_filters[i],
+                      kernel_size=filter_sizes[i])
+            for i in range(len(filter_sizes))
+        ])
+        # Fully-connected layer and Dropout
+        self.fc = nn.Linear(np.sum(num_filters), num_classes)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, input_ids):
+
+        # Output shape: (b, max_len, embed_dim)
+        x_embed = self.embedding(input_ids).float()
+
+        # Output shape: (b, embed_dim, max_len)
+        x_reshaped = x_embed.permute(0, 2, 1)
+
+        # Output shape: (b, num_filters[i], L_out)
+        x_conv_list = [F.relu(conv1d(x_reshaped)) for conv1d in self.conv1d_list]
+
+        # Output shape: (b, num_filters[i], 1)
+        x_pool_list = [F.max_pool1d(x_conv, kernel_size=x_conv.shape[2])
+                       for x_conv in x_conv_list]
+
+        # Output shape: (b, sum(num_filters))
+        x_fc = torch.cat([x_pool.squeeze(dim=2) for x_pool in x_pool_list],
+                         dim=1)
+
+        # TODO: might not needed
+        # logits = self.fc(self.dropout(x_fc))
+
+        return x_fc
